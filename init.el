ffd75bac79297a6499395522d4eaf52d1d5422dd
Benjamin Turner <benjamin@passionsplay.com>
2018-10-24 21:26:11 -0700

    Add basic config

---
+++ init.el
@@ -0,0 +1,268 @@
+;; Minimal UI
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(menu-bar-mode -1)
+
+(add-to-list 'default-frame-alist '(font . "Ubuntu Mono-16"))
+(add-to-list 'default-frame-alist '(height . 24))
+(add-to-list 'default-frame-alist '(width . 80))
+
+;; Package configs
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+;; Vim mode
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
+
+;; Theme
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-one t))
+
+;; Fancy titlebar for MacOS
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
+(setq ns-use-proxy-icon  nil)
+(setq frame-title-format nil)
+
+;; Helm
+(use-package helm
+  :ensure t
+  :init
+  (setq helm-M-x-fuzzy-match t
+  helm-mode-fuzzy-match t
+  helm-buffers-fuzzy-matching t
+  helm-recentf-fuzzy-match t
+  helm-locate-fuzzy-match t
+  helm-semantic-fuzzy-match t
+  helm-imenu-fuzzy-match t
+  helm-completion-in-region-fuzzy-match t
+  helm-candidate-number-list 150
+  helm-split-window-in-side-p t
+  helm-move-to-line-cycle-in-source t
+  helm-echo-input-in-header-line t
+  helm-autoresize-max-height 0
+  helm-autoresize-min-height 20)
+  :config
+  (helm-mode 1))
+
+;; RipGrep
+(use-package helm-rg :ensure t)
+
+;; Allow Helm to narrow M-x commands
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+;; Which Key
+(use-package which-key
+  :ensure t
+  :init
+  (setq which-key-separator " ")
+  (setq which-key-prefix-prefix "+")
+  :config
+  (which-key-mode 1))
+
+;; Custom keybinding
+(use-package general
+  :ensure t
+  :config (general-define-key
+  :states '(normal visual insert emacs)
+  :prefix "SPC"
+  :non-normal-prefix "M-SPC"
+  ;; "/"   '(counsel-rg :which-key "ripgrep") ; You'll need counsel package for this
+  "TAB" '(switch-to-prev-buffer :which-key "previous buffer")
+  "SPC" '(helm-M-x :which-key "M-x")
+  "pf"  '(helm-projectile-find-file :which-key "find files")
+  "pp"  '(helm-projectile-switch-project :which-key "switch project")
+  "pb"  '(helm-projectile-switch-to-buffer :which-key "switch buffer")
+  "pr"  '(helm-show-kill-ring :which-key "show kill ring")  ;; Buffers
+  "bb"  '(helm-buffers-list :which-key "buffers list")
+  ;; Window
+  "wl"  '(windmove-right :which-key "move right")
+  "wh"  '(windmove-left :which-key "move left")
+  "wk"  '(windmove-up :which-key "move up")
+  "wj"  '(windmove-down :which-key "move bottom")
+  "w/"  '(split-window-right :which-key "split right")
+  "w-"  '(split-window-below :which-key "split bottom")
+  "wx"  '(delete-window :which-key "delete window")
+  ;; Others
+  "at"  '(ansi-term :which-key "open terminal")
+  "aoa"  '(org-agenda :which-key "org agenda")
+))
+
+;; Powerline
+(use-package spaceline
+  :ensure t
+  :init
+  (setq powerline-default-separator 'slant)
+  :config
+  (spaceline-emacs-theme)
+  (spaceline-toggle-minor-modes-off)
+  (spaceline-toggle-buffer-size-off)
+  (spaceline-toggle-evil-state-on))
+
+;; Projectile
+(use-package projectile
+  :ensure t
+  :init
+  (setq projectile-require-project-root nil)
+  :config
+  (projectile-mode 1))
+
+;; Helm Projectile
+(use-package helm-projectile
+  :ensure t
+  :init
+  (setq helm-projectile-fuzzy-match t)
+  :config
+  (helm-projectile-on))
+
+;; OrgMode Configs
+;; Define Global Orgmode keybindings
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-switchb)
+
+(setq org-return-follows-link t)
+(setq org-html-validation-link nil)
+(use-package org-bullets
+  :ensure t
+  :init
+  (add-hook 'org-mode-hook (lambda ()
+			    (org-bullets-mode 1)
+			    (setq buffer-face-mode-face '(:family "Ubuntu Mono" :height 1.0))
+			    ;; (hide-mode-line-mode 1)
+			    (buffer-face-mode))))
+
+;; set line wrap for text modes
+(add-hook 'text-mode-hook #'visual-line-mode)
+
+;; Define default capture file
+(setq org-default-notes-file "~/org/capture.org")
+
+;; Define capture templates
+(setq org-capture-templates
+	'(
+
+	("b" "Templates for Ben")
+	("bt" "Todo" entry (file+headline "~/org/life.org" "Tasks")
+	    "* TODO %?\n  %i\n  %a")
+
+	("p" "Templates for PassionsPlay")
+	("pt" "Todo" entry (file+headline "~/org/passionsplay.org" "Tasks")
+	    "* TODO %?\n  %i\n  %a")
+
+	("f" "Templates for FW")
+	("ft" "Todo" entry(file+headline "~/org/fw.org" "Todo")
+	    "* TODO %?\n %i\n %a")
+
+	))
+
+(eval-after-load 'org-agenda
+ '(progn
+    (evil-set-initial-state 'org-agenda-mode 'normal)
+    (evil-define-key 'normal org-agenda-mode-map
+      (kbd "<RET>") 'org-agenda-switch-to
+      (kbd "\t") 'org-agenda-goto
+
+      "q" 'org-agenda-quit
+      "r" 'org-agenda-redo
+      "S" 'org-save-all-org-buffers
+      "gj" 'org-agenda-goto-date
+      "gJ" 'org-agenda-clock-goto
+      "gm" 'org-agenda-bulk-mark
+      "go" 'org-agenda-open-link
+      "s" 'org-agenda-schedule
+      "+" 'org-agenda-priority-up
+      "," 'org-agenda-priority
+      "-" 'org-agenda-priority-down
+      "y" 'org-agenda-todo-yesterday
+      "n" 'org-agenda-add-note
+      "t" 'org-agenda-todo
+      ":" 'org-agenda-set-tags
+      ";" 'org-timer-set-timer
+      "I" 'helm-org-task-file-headings
+      "i" 'org-agenda-clock-in-avy
+      "O" 'org-agenda-clock-out-avy
+      "u" 'org-agenda-bulk-unmark
+      "x" 'org-agenda-exit
+      "j"  'org-agenda-next-line
+      "k"  'org-agenda-previous-line
+      "vt" 'org-agenda-toggle-time-grid
+      "va" 'org-agenda-archives-mode
+      "vw" 'org-agenda-week-view
+      "vl" 'org-agenda-log-mode
+      "vd" 'org-agenda-day-view
+      "vc" 'org-agenda-show-clocking-issues
+      "g/" 'org-agenda-filter-by-tag
+      "o" 'delete-other-windows
+      "gh" 'org-agenda-holiday
+      "gv" 'org-agenda-view-mode-dispatch
+      "f" 'org-agenda-later
+      "b" 'org-agenda-earlier
+      "c" 'helm-org-capture-templates
+      "e" 'org-agenda-set-effort
+      "n" nil  ; evil-search-next
+      "{" 'org-agenda-manipulate-query-add-re
+      "}" 'org-agenda-manipulate-query-subtract-re
+      "A" 'org-agenda-toggle-archive-tag
+      "." 'org-agenda-goto-today
+      "0" 'evil-digit-argument-or-evil-beginning-of-line
+      "<" 'org-agenda-filter-by-category
+      ">" 'org-agenda-date-prompt
+      "F" 'org-agenda-follow-mode
+      "D" 'org-agenda-deadline
+      "H" 'org-agenda-holidays
+      "J" 'org-agenda-next-date-line
+      "K" 'org-agenda-previous-date-line
+      "L" 'org-agenda-recenter
+      "P" 'org-agenda-show-priority
+      "R" 'org-agenda-clockreport-mode
+      "Z" 'org-agenda-sunrise-sunset
+      "T" 'org-agenda-show-tags
+      "X" 'org-agenda-clock-cancel
+      "[" 'org-agenda-manipulate-query-add
+      "g\\" 'org-agenda-filter-by-tag-refine
+      "]" 'org-agenda-manipulate-query-subtract)))
+
+;; Show matching parens
+(setq show-paren-delay 0)
+(show-paren-mode 1)
+
+;; Disable backup files
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+
+;; Splash Screen
+(setq inhibit-startup-screen t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/Documents" )))
+ '(package-selected-packages (quote (neotree general which-key helm evil use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

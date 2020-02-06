3486b88a253680125437a0325ef37423cabf0791
Benjamin Turner <benjamin@passionsplay.com>
2020-02-05 19:45:03 -0600

    Enable webmode for blade partials

--- init.el
+++ init.el
@@ -484,6 +484,7 @@ is possible if the heading has a property of DATE_TREE."
 
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\.twig\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 

77273e7c117b6d1333b288a7073c08645abb2446
Benjamin Turner <benjamin@passionsplay.com>
2021-04-01 14:54:51 -0700

    Add minimal Mac bootstrap file so I remember the tools I install

---
+++ bootstrap-mac.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#
+# CLI tooling
+#
+echo "☻ Installing CLI tools..."
+brew install coreutils
+brew install --cask syncthing
+brew install ispell
+brew install ffmpeg
+brew install imagemagick
+brew install grep
+brew install ripgrep
+brew install pup
+brew install librsvg
+brew install jq
+brew install fzf
+brew install asciinema
+
+#
+# GUI
+#
+echo "☻ Installing GUI tools..."
+# Fonts and themes
+brew install font-fira-code
+
+# Productivity
+brew install amethyst
+brew install 1password
+
+# Browsers
+brew install firefox
+brew install brave-browser
+
+# OSS graphic design
+brew install inkscape
+brew install gimp
+
+# Media
+brew install vlc
+brew install obs
+
+# Install Emacs
+echo "☻ Installing Emacs..."
+brew tap d12frosted/emacs-plus && \
+    brew install emacs-plus && \
+    ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications/Emacs.app

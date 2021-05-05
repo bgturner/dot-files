713ef4d0e52ea1f8b1780e90b4948992083d661c
Benjamin Turner <benjamin@passionsplay.com>
2021-05-05 15:06:20 -0700

    Make dired work under mac like it does under linux

--- init.el
+++ init.el
@@ -272,9 +272,18 @@
    "glf" '(magit-log-buffer-file :which-key "Current File")))
 
 
+;; Dired
+(when (eq system-type 'darwin)
+  ;; installed with `brew install coreutils`
+  ;; use proper GNU ls
+  (setq insert-directory-program "/usr/local/bin/gls"))
+
 (use-package dired+
-  :init
-    (setq dired-listing-switches "-aghBo --group-directories-first"))
+    :init
+    (progn
+	;; Details toggling is bound to "(" in `dired-mode' by default
+	(setq diredp-hide-details-initially-flag t)
+	(setq dired-listing-switches "-aghBo --group-directories-first")))
 
 (use-package dired-hide-dotfiles
   :hook (dired-mode . dired-hide-dotfiles-mode)

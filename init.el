6bbc1c7b1ffcb4973be95af9d9f28748fc60d347
Benjamin Turner <benjamin@passionsplay.com>
2021-11-19 14:00:50 -0800

    Install ansi-color so that compilation buffers look good

--- init.el
+++ init.el
@@ -381,6 +381,13 @@
 (setq ns-use-proxy-icon  nil)
 (setq frame-title-format nil)
 
+(use-package ansi-color
+  :config
+  (defun my-colorize-compilation-buffer ()
+    (when (eq major-mode 'compilation-mode)
+      (ansi-color-apply-on-region compilation-filter-start (point-max))))
+  :hook (compilation-filter . my-colorize-compilation-buffer))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Apps
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

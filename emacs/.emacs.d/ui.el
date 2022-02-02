7aa0ae9d892f9af87b51dedacea724ddec13a9f0
Benjamin Turner <benjamin@passionsplay.com>
2022-02-02 11:29:41 -0800

    Make emacs remember recently visited files

--- emacs/.emacs.d/ui.el
+++ emacs/.emacs.d/ui.el
@@ -10,6 +10,8 @@
 ;; Remember window layouts
 (winner-mode 1)
 
+(recentf-mode)
+
 (setq initial-scratch-message "")         ; Make *scratch* buffer blank
 (setq ring-bell-function 'ignore)         ; Disable bell sound
 (fset 'yes-or-no-p 'y-or-n-p)             ; y-or-n-p makes answering questions faster

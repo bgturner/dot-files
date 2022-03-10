9601c3e0ff41662309a20ce72bbd0a0317bcc236
Benjamin Turner <benjamin@passionsplay.com>
2022-03-09 16:12:10 -0800

    update capture templates for work related tasks

--- emacs/.emacs.d/.org-capture-templates.el
+++ emacs/.emacs.d/.org-capture-templates.el
@@ -36,20 +36,13 @@
 :time-prompt t
 :tree-type week)
 
+
 ("p" "Planning")
 ("pd" "Daily"
  entry (file+olp+datetree "~/syncthing/WPE/org/journal.org")
 "
 * Standup :meetings:local:scrum:
   %(org-insert-time-stamp (org-read-date nil t \"%T\") nil nil nil nil \" 07:00-07:15\")
-* Slack :misc:slack:
-  %(org-insert-time-stamp (org-read-date nil t \"%T\") nil nil nil nil \" 07:15\")
-* Daily Planning :planning:
-  %(org-insert-time-stamp (org-read-date nil t \"%T\") nil nil nil nil \" 07:15\")
-* Lunch :breaks:
-  %(org-insert-time-stamp (org-read-date nil t \"%T\") nil nil nil nil \" 12:30-13:30\")
-* Daily Review :planning:
-  %(org-insert-time-stamp (org-read-date nil t \"%T\") nil nil nil nil \" 14:30-15:00\")
 "
     :time-prompt t)
 
@@ -78,6 +71,22 @@
  entry (file+olp+datetree
 	"~/syncthing/WPE/org/local/task-clocking/slack-support.org")
 "* %^C\n  %?"
+:clock-in t
+:clock-resume t
+:tree-type week)
+
+("ld" "Dev Blocks")
+("ldp" "Pull Requests"
+ entry (file+olp+datetree
+	"~/syncthing/WPE/org/pull-requests.org"
+	"Clocking")
+"* LWP Site\n  %T\n  %?"
+:time-prompt t
+:tree-type week)
+("ldr" "Pair Programming"
+ entry (file+olp+datetree
+	"~/syncthing/WPE/org/pair-programming.org" "Clocking")
+"* %^{Pairing}\n  %?"
 :clock-in t
 :clock-resume t
 :tree-type week)

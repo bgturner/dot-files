beec545d7b200ab66c05ced2db87f3b0b8d5aa9e
Benjamin Turner <benjamin@passionsplay.com>
2022-06-21 20:26:17 -0700

    feat: add config settings in link-config.sh for VS Code under linux

--- vscode/link-config.sh
+++ vscode/link-config.sh
@@ -5,6 +5,8 @@ dot_file_folder="$(pwd)"
 function goto_config_dir {
   mac_vscode_config="${HOME}/Library/Application Support/Code/User" 
   [ -d  "$mac_vscode_config" ] && cd "$mac_vscode_config" && echo "Changed to ${mac_vscode_config}"
+  linux_vscode_config="${HOME}/.config/Code/User"
+  [ -d  "$linux_vscode_config" ] && cd "$linux_vscode_config" && echo "Changed to ${linux_vscode_config}"
 }
 
 function backup_configs {

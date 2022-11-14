c5d5453adb71a6db2dc2cc190266aaeffbd287c6
Benjamin Turner <ben.turner@wpengine.com>
2022-11-14 09:44:36 -0800

    wip: elisp functions for compiling Local

--- emacs/.emacs.d/work-config.el
+++ emacs/.emacs.d/work-config.el
@@ -28,3 +28,35 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  '(default ((t (:height 140 :family "JetBrains Mono")))))
+
+;; Local specific functions
+(defun bt/local-compilation ()
+  "Spawns two compilation buffers to launch Local."
+  (interactive)
+  ;; Ensure that the general org inbox is part of our agenda
+  ;; (projectile-switch-project-by-name "flywheel-local")
+  ;; (bt/kill-local-compilation)
+  (projectile-dired)
+  ;; (make-frame)
+  (compile "yarn nps build.dev")
+  (switch-to-buffer "*compilation*")
+  (rename-buffer "*compilation-local-nps-build.dev*")
+  (switch-to-buffer "flywheel-local")
+  (compile "yarn nps")
+  (switch-to-buffer "*compilation*")
+  (rename-buffer "*compilation-local-nps*")
+  (switch-to-buffer "*compilation-local-nps-build.dev*"))
+
+(defun bt/kill-local-compilation ()
+  "Kills the two compilation buffers related to Local."
+  (interactive)
+  (kill-buffer "*compilation-local-nps-build.dev*")
+  (kill-buffer "*compilation-local-nps*"))
+
+(defun bt/open-local-config-folder ()
+  "Opens a Dired buffer of the Local config folder"
+  (interactive)
+  (dired "~/Library/Application Support/Local/" nil))
+
+
+;; (global-set-key (kbd "C-c C-l") 'counsel-find-file)

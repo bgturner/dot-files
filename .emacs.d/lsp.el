8a19850565a5102d7cec433d40cf9d2bf778a2d7
Benjamin Turner <ben.turner@wpengine.com>
2022-11-28 21:56:36 -0800

    fix: add function to clear lsp-workspace-folders

--- .emacs.d/lsp.el
+++ .emacs.d/lsp.el
@@ -2,6 +2,21 @@
 (use-package lsp-mode
   :config
   (lsp-enable-which-key-integration t)
+  (defun bt/clear-lsp-workspace-folders ()
+"Clears the registered workspaces for the current lsp-session.
+
+I was getting 'Creating Pip: Too many files open' errors. It turns out
+I had too many projects registered along with too many projects open
+at one time.
+
+Running this occasionally zeros out repos that I no longer need so
+that I can re-add any projects that I'm actively working on. See:
+- https://emacs.stackexchange.com/q/52967/20510
+- https://www.reddit.com/r/emacs/comments/ty5i2l/emacs_28_29_watching_eln_files_and_causing_too/
+"
+    (interactive)
+    (dolist (workspace (lsp-session-folders (lsp-session)))
+      (lsp-workspace-folders-remove workspace)))
   :hook
   (php-mode . lsp)
   (typescript-mode . lsp)

156ca524fbb7f718e727c90adbce522b81f8c5ed
Benjamin Turner <ben.turner@wpengine.com>
2023-01-22 08:15:00 -0800

    feat: better settings for tree-sitter

--- .emacs.d/general-programming.el
+++ .emacs.d/general-programming.el
@@ -187,16 +187,15 @@
   :config
   (global-wakatime-mode))
 
-(use-package tree-sitter)
-(use-package tree-sitter-langs)
-
-(use-package codespaces
-  :straight (:host github
-	     :repo "patrickt/codespaces.el")
-  :init (setq vc-handled-backends '(Git))
-  :config (codespaces-setup)
-  :bind ("C-c S" . #'codespaces-connect))
-
-(use-package company-tabnine :ensure t
+(use-package tree-sitter
+  :delight
   :config
-  (add-to-list 'company-backends #'company-tabnine))
+  ;; activate tree-sitter on any buffer containing code for which it has a parser available
+  (global-tree-sitter-mode)
+  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
+  ;; by switching on and off
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :after tree-sitter)
+

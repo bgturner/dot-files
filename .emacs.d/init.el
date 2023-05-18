22662b42c39d70dbd3cab0e3ce5e1c7a4fa98297
Benjamin Turner <benjamin@passionsplay.com>
2023-05-17 19:07:44 -0700

    remove elfeed

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -995,43 +995,6 @@ that I can re-add any projects that I'm actively working on. See:
     :defer t
     :init (evil-set-initial-state 'pocket-reader-mode 'emacs))
   
-  ;; (use-package elfeed
-  ;;   :defer t
-  ;;   :init
-  ;;   (evil-set-initial-state 'elfeed-search-mode 'emacs)
-  ;;   (evil-set-initial-state 'elfeed-show-mode 'emacs)
-  ;;   (setq elfeed-db-directory "~/org/personal/rss/.elfeed")
-  ;;   :bind
-  ;;   (:map elfeed-search-mode-map
-  ;;         ("q" . pp/elfeed-save-db-and-bury))
-  ;;   ;; Elfeed News
-  ;;   (use-package elfeed-org
-  ;;     :defer t
-  ;;     :init
-  ;;     (setq rmh-elfeed-org-files (list "~/org/personal/rss/rss-feeds.org"))
-  ;;     :config
-  ;;     (elfeed-org))
-  ;;   (use-package elfeed-score
-  ;;     :defer t
-  ;;     :init
-  ;;     (setq elfeed-score-score-file "~/org/personal/rss/elfeed.score")
-  ;;     :config
-  ;;     (progn
-  ;; 	(elfeed-score-enable)
-  ;; 	(define-key elfeed-search-mode-map "=" elfeed-score-map)))
-  ;;   (defun pp/elfeed-load-db-and-open ()
-  ;;     "Wrapper to load the elfeed db from disk before opening."
-  ;;     (interactive)
-  ;;     (elfeed-db-load)
-  ;;     (elfeed)
-  ;;     (elfeed-search-update--force))
-  ;;   (defun pp/elfeed-save-db-and-bury ()
-  ;;     "Wrapper to save the elfeed db to disk before burying buffer."
-  ;;     (interactive)
-  ;;     (elfeed-db-save)
-  ;;     (quit-window))
-  ;;   )
-
   (provide 'apps))
 
 

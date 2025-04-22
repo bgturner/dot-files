7a4289bddada9b871f2517080f9e5e22e61f52d0
Benjamin Turner <benjamin@passionsplay.com>
2025-04-21 21:20:24 -0700

    feat: add a few basic emacs settings

--- .emacs.d.bare/init.el
+++ .emacs.d.bare/init.el
@@ -13,9 +13,27 @@
 
 (global-set-key [remap list-buffers] 'ibuffer)
 
+(use-package emacs
+  :init
+  (setq-default tab-width 4)
+  (setq-default indent-tabs-mode nil)
+  (setq inhibit-startup-screen t)
+  :config
+  (toggle-scroll-bar -1)
+  (tool-bar-mode -1)
+  (tooltip-mode -1)
+  (scroll-bar-mode -1)
+  (blink-cursor-mode -1)
+  (line-number-mode +1)
+  (column-number-mode t)
+  (size-indication-mode t)
+  (fset 'yes-or-no-p 'y-or-n-p)
+  (load-theme 'misterioso))
+
 (use-package evil
   :config
   (evil-set-undo-system 'undo-redo)
   (evil-mode 1)
   (evil-define-key 'normal 'global "]q" 'next-error)
   (evil-define-key 'normal 'global "[q" 'previous-error))
+

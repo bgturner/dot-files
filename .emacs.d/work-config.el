5b5ab6e9d225a5dae169d6cfae300c1e98b13141
Benjamin Turner <ben.turner@wpengine.com>
2025-09-09 11:36:34 -0700

    refactor: make Local compile elisp functions more flexible

--- .emacs.d/work-config.el
+++ .emacs.d/work-config.el
@@ -30,22 +30,36 @@
  '(default ((t (:height 140 :family "JetBrains Mono")))))
 
 ;; Local specific functions
+(defconst bt/local-compilation-build-cmd "yarn nps build.dev")
+(defconst bt/local-compilation-default-cmd "yarn nps")
+(defconst bt/local-compilation-debug-cmd "yarn nps debug")
+
+(defun bt/run-compile (cmd &optional buffer-name)
+  "Run a compilation command and rename the buffer if specified.
+    CMD is the command to run, BUFFER-NAME is the new name for the compilation buffer."
+  (compile cmd)
+  (switch-to-buffer "*compilation*")
+  (when buffer-name
+    (rename-buffer buffer-name)))
+
+
+;; Local specific functions
 (defun bt/local-compilation ()
   "Spawns two compilation buffers to launch Local."
   (interactive)
-  ;; Ensure that the general org inbox is part of our agenda
-  ;; (projectile-switch-project-by-name "flywheel-local")
-  ;; (bt/kill-local-compilation)
-  (projectile-dired)
-  ;; (make-frame)
-  (compile "yarn nps build.dev")
-  (switch-to-buffer "*compilation*")
-  (rename-buffer "*compilation-local-nps-build.dev*")
-  (switch-to-buffer "flywheel-local")
-  (compile "yarn nps")
-  (switch-to-buffer "*compilation*")
-  (rename-buffer "*compilation-local-nps*")
-  (switch-to-buffer "*compilation-local-nps-build.dev*"))
+  (let ((nps-cmd (if current-prefix-arg bt/local-compilation-debug-cmd bt/local-compilation-default-cmd)))
+    (projectile-dired)  ; Open projectile dired
+    ;; Run build.dev compilation
+    (bt/run-compile bt/local-compilation-build-cmd "*compilation-local-nps-build.dev*")
+
+    ;; Switch to flywheel-local directory
+    (switch-to-buffer "flywheel-local")
+
+    ;; Run nps or nps debug compilation based on prefix argument
+    (bt/run-compile nps-cmd (if current-prefix-arg "*compilation-local-nps-debug*" "*compilation-local-nps*"))
+
+    ;; Switch back to build.dev compilation buffer
+    (switch-to-buffer "*compilation-local-nps-build.dev*")))
 
 (defun bt/kill-local-compilation ()
   "Kills the two compilation buffers related to Local."

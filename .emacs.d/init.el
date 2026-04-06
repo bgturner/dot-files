f3f9b621e8c5516f38d71a0647f0f645f05739b0
Benjamin Turner <benjamin@passionsplay.com>
2026-04-06 14:22:46 -0700

    emacs: remove call to custom bt/set-org-agenda-files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1171,8 +1171,6 @@ that I can re-add any projects that I'm actively working on. See:
 				                 (js . t)))
   
   (load-user-file ".org-capture-templates.el")
-
-  (bt/set-org-agenda-files)
   
   (defun org-refile-to-datetree (&optional file)
     "Refile a subtree to a datetree corresponding to it's timestamp.
